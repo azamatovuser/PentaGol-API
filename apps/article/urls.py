e7c6ac2ca2e843cf1371dd5cff8012a060ff3664
aazamatoff <jonh1234554321@gmail.com>
2023-04-30 15:28:23 +0500

    four article

--- apps/article/urls.py
+++ apps/article/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ArticleAllListAPIView, ArticleLastListAPIView, ArticleRetrieveAPIView
+from .views import ArticleAllListAPIView, ArticleLastListAPIView, ArticleRetrieveAPIView, ArticleLastFourListAPIView
 
 
 urlpatterns = [
     path('list_all/', ArticleAllListAPIView.as_view()),
     path('list_last/', ArticleLastListAPIView.as_view()),
+    path('list_last_four/', ArticleLastFourListAPIView.as_view()),
     path('detail/<int:pk>/', ArticleRetrieveAPIView.as_view()),
 ]
--- apps/article/views.py
+++ apps/article/views.py
@@ -13,6 +13,11 @@ class ArticleLastListAPIView(generics.ListAPIView):
     serializer_class = ArticleSerializer
 
 
+class ArticleLastFourListAPIView(generics.ListAPIView):
+    queryset = Article.objects.all().order_by('-id')[:4]
+    serializer_class = ArticleSerializer
+
+
 class ArticleRetrieveAPIView(generics.RetrieveAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
